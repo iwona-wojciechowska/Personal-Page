bd1ce51472801e68dd1abac06e4ff0f9992fa6c8
Iwona Wojciechowska <iwona_wojciechowska@Air-Iwona.lan>
2019-09-26 20:21:08 +0200

    my project

---
+++ main.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector(".burger");
+const activeElement = document.querySelectorAll(".active");
+
+burger.addEventListener("click", function () {
+    for(let i = 0; i < activeElement.length; i++) {
+        activeElement[i].classList.toggle("show")
+    }
+})
+
+
+
